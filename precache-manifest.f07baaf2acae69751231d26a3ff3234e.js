172ad84fd856e785fb414a0d29f60e53edfc3c89
Jason Bonilla <jason@jasons-air-2.attlocal.net>
2021-06-15 15:05:56 -0700

    Updates

---
+++ precache-manifest.f07baaf2acae69751231d26a3ff3234e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7f2d3cdac1c2126806b",
+    "url": "/me/static/css/main.2174eee7.chunk.css"
+  },
+  {
+    "revision": "c7f2d3cdac1c2126806b",
+    "url": "/me/static/js/main.c7f2d3cd.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3d61f067466ce6738b352a1c494e0445",
+    "url": "/me/index.html"
+  }
+];
